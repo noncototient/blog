eb613598151b87ce3af11488f063b9bfcf3d5b37
Bo <bogdan.kurbetyev@gmail.com>
2017-05-24 19:28:47 +1000

    Unauthenticated user cannot create post test pass

--- tests/Feature/CreatePostTest.php
+++ tests/Feature/CreatePostTest.php
@@ -12,7 +12,8 @@ class CreatePostTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function an_authenticated_user_can_create_a_post() {
+    public function an_authenticated_user_can_create_a_post()
+    {
         // Given we have a user...
         $user = factory(\App\User::class)->create();
 
@@ -23,12 +24,54 @@ class CreatePostTest extends TestCase
         ]);
 
         // And submits the create post form...
-        $response = $this->withExceptionHandling()->actingAs($user)->post(route('post.store'), $post);
-        
+        $response = $this->actingAs($user)->post(route('post.store'), $post);
+
         // Should result in a post being stored in the database
         $this->assertDatabaseHas('posts', ['title' => $post['title'], 'body' => $post['body']]);
 
         // And redirect the user with a success message
         $response->assertSessionHas('success', 'Your post has been successfully published.')->assertStatus(302);
     }
+
+    /** @test */
+    public function an_authenticated_user_cannot_create_a_post_with_empty_fields()
+    {
+        // Given we have a user...
+        $user = factory(\App\User::class)->create();
+
+        // Who creates a post and misses title and the body
+        $post = factory(\App\Post::class)->raw([
+            'user_id' => $user->id,
+            'title' => null,
+            'body' => null,
+            'active' => true
+        ]);
+
+        // And then submits the create post form...
+        $response = $this->withExceptionHandling()->actingAs($user)->post(route('post.store'), $post);
+
+        // Should result in the database not saving the data
+        $this->assertDatabaseMissing('posts', ['title' => $post['title']]);
+
+        // As well as redirecting the user
+        $response->assertStatus(302);
+    }
+
+    /** @test */
+    public function an_unauthenticated_user_cannot_create_a_post()
+    {
+        // Given we have a user...
+        factory(\App\User::class)->create();
+
+        // Who somehow figures out the pattern to create a post without a form...
+        $post = factory(\App\Post::class)->raw([
+            'user_id' => 1
+        ]);
+
+        // But is not authenticated and tries to send the data to the server...
+        // Should result in redirection to 'login'
+        $this->withExceptionHandling()
+            ->post(route('post.store'), $post)
+            ->assertRedirect('/login');
+     }
 }
